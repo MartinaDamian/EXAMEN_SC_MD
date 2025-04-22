0fadc626b0e827c32096ca9f5951e6d2acfbc793
MartinaDamian <martinadamian5151@gmail.com>
2025-04-22 16:15:24 -0500

    inicio

---
+++ src/Pokemon.java
@@ -0,0 +1,10 @@
+import java.lang.ref.PhantomReference;
+
+public class Pokemon {
+    private String nombre;
+    private String tipo;
+    private int nivelPoder;
+    private String habilidad;
+    private String estado;
+
+}
