6fa3eaa911698291b480292c39a773f50e299a1f
Sara Chiriboga <sara.chiriboga.valle@udla.edu.ec>
2025-04-22 16:58:46 -0500

    cola lista

--- src/ColaPokemon.java
+++ src/ColaPokemon.java
@@ -5,12 +5,17 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class ColaPokemon {
-    Queue<Pokemon> pokemones = new LinkedList<>(); //cola con pokemones ingresados
+    LinkedList<Pokemon> pokemones = new LinkedList<>();
     private Pokemon predet;
 
-    //metodos solicitados
-    public void encolarPokemon(JTextArea cont, ){
+    //constructor
+    public ColaPokemon() {
+        pokemones.add(predet);
+    }
 
+    //metodos solicitados
+    public void encolarPokemon(JTextArea cont, JTextField nombre, JTextField tipo, JTextField nivelPoder, JFormattedTextField habilidad, JTextField estado){
+        Pokemon nuevo = new Pokemon(nombre.getText(), tipo.getText(), Integer.parseInt(nivelPoder.getText()), habilidad.getText(), estado.getText()); //nuevo pokemon
     }
 
 
