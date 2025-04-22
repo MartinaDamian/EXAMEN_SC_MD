4ebeff71b8dbba4c78e09702794fc340191a32d4
MartinaDamian <martinadamian5151@gmail.com>
2025-04-22 16:28:49 -0500

    pokemon inicio

--- src/Pokemon.java
+++ src/Pokemon.java
@@ -7,4 +7,41 @@ public class Pokemon {
     private String habilidad;
     private String estado;
 
+    public Pokemon(String nombre, String tipo, int nivelPoder, String habilidad, String estado) {
+        this.nombre = "Pikachu";
+        this.tipo = "Eléctrico";
+        this.nivelPoder = 120;
+        this.habilidad = "Impactrueno";
+        this.estado = "Activo";
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public int getNivelPoder() {
+        return nivelPoder;
+    }
+
+    public String getHabilidad() {
+        return habilidad;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+
+        return super.toString();
+    }
 }
