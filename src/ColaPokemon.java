e15d6942dcc83f267dcb815b1f1147d3d3baa7eb
Sara Chiriboga <sara.chiriboga.valle@udla.edu.ec>
2025-04-22 16:34:56 -0500

    ColaPokemon inicio

--- src/ColaPokemon.java
+++ src/ColaPokemon.java
@@ -1,2 +1,18 @@
+import javax.swing.*;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class ColaPokemon {
+    Queue<Pokemon> pokemones = new LinkedList<>(); //cola con pokemones ingresados
+    private Pokemon predet;
+
+    //metodos solicitados
+    public void encolarPokemon(JTextArea cont, ){
+
+    }
+
+
+
 }
