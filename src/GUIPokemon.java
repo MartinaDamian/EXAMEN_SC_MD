ce243b83968792b663fd63dc67f8a3843264c06b
MartinaDamian <martinadamian5151@gmail.com>
2025-04-22 20:32:15 -0500

    lista

--- src/GUIPokemon.java
+++ src/GUIPokemon.java
@@ -6,4 +6,9 @@ public class GUIPokemon {
     private JButton limpiarButton;
     private JButton ajusteDePoderButton;
     private JButton filtrarButton;
+    private JTextField nombretxt;
+    private JTextField txtTipo;
+    private JTextField txtPoder;
+    private JTextField txtHabilidad;
+    private JTextField txtEstado;
 }
